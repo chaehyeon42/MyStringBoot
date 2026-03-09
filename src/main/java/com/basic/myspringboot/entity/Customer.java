49aa80f405fce701ea727000d2c926fae411f582
chaehyeon42 <qpqp8899@naver.com>
2026-03-09 15:25:29 +0900

    Customer Entity add

---
+++ src/main/java/com/basic/myspringboot/entity/Customer.java
@@ -0,0 +1,22 @@
+package com.basic.myspringboot.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity //테이블하고 매핑되는 class
+@Table(name = "customers") //테이블 명 지정
+@Getter @Setter
+public class Customer {
+    //@Id = Customer테이블의 Primary Key를 나타내는 id 어노테이션
+    //@GeneratedValue =  SEQUENCE한 값을 자동으로 생성
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String customerId;
+
+    @Column(nullable = false)
+    private String customerName;
+}
--- src/main/java/com/basic/myspringboot/runner/DatabaseRunner.java
+++ src/main/java/com/basic/myspringboot/runner/DatabaseRunner.java
@@ -14,9 +14,10 @@ import java.sql.DatabaseMetaData;
 //Spring Data : In Memory 데이터베이스
 //H2 데이터베이스 기본 연결 정보 확인
 @Component
-@Order(1)
-@Slf4j
+@Order(1) //해당 데이터베이스 오더가 1이므로 MyRunner.java보다 우선순위가 높음
+@Slf4j //롬복에 해당 어노테이션을 선언하면 로거 객체를 생성 해줘서 따로 작성 안해도 됨.
 public class DatabaseRunner implements ApplicationRunner {
+   //데이터소스를 인젝션 받음
     @Autowired
     DataSource dataSource;
 
@@ -24,11 +25,13 @@ public class DatabaseRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         log.info("DataSource 구현객체는 {} ", dataSource.getClass().getName());
 
+        //sql의 Connection
         try (Connection connection = dataSource.getConnection()) {
-            DatabaseMetaData metaData = connection.getMetaData();
+            DatabaseMetaData metaData = connection.getMetaData(); //db 메타 데이터 가져옴
             log.info("DB Product Name = {}", metaData.getDatabaseProductName());
             log.info("DB URL = {}", metaData.getURL());
             log.info("DB Username = {}", metaData.getUserName());
+            log.info("DB Producer version={}",metaData.getDatabaseProductVersion());
         }
     }
 }
