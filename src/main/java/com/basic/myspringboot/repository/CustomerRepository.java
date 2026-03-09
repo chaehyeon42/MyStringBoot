c234c3905e41eb7e40244d07aad210e79086f8bc
chaehyeon42 <qpqp8899@naver.com>
2026-03-09 16:53:46 +0900

    Repository Method add

--- src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
+++ src/main/java/com/basic/myspringboot/repository/CustomerRepository.java
@@ -3,6 +3,14 @@ package com.basic.myspringboot.repository;
 import com.basic.myspringboot.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    // finder(query) method
+    //1. customerID(고객번호, Unique - 중복불가)를 조회하는 finder 메서드
+    Optional<Customer> finByCustomerId(String customerId);
+
+    //2.CustomerName(고객명, 중복허용)로 조회하는 finder 메서드
+    List<Customer> findByCustomerName(String customerName);
 }
