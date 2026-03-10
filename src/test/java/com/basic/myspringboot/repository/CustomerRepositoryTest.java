1da4a1026e4b7ca9f853158566154eefda5f27cb
chaehyeon42 <qpqp8899@naver.com>
2026-03-10 13:32:58 +0900

    CustomerRepositoryTest Find add

--- src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/repository/CustomerRepositoryTest.java
@@ -2,11 +2,14 @@ package com.basic.myspringboot.repository;
 
 import com.basic.myspringboot.entity.Customer;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +23,7 @@ class CustomerRepositoryTest {
     //1. Customer 등록
     @Test
     @Rollback(value = false) // @Transactional시 DB에 반영 해달라는 어노테이션-롤백 처리 하지 않도록
+    @Disabled //잠깐 일시정지(실행 안되도록) 어노테이션
     void testCreate() {
         //Given(준비단계)
         Customer customer = new Customer();
@@ -32,4 +36,39 @@ class CustomerRepositoryTest {
         assertThat(addCustomer.getCustomerName()).isEqualTo("스프링부트4");
     }
 
+    //2. Customer 조회 - 정상인경우
+    @Test
+    void testFindBy() {
+        Optional<Customer> optionalCustomer = customerRepository.findById(1L); //정수와 구분하기 위해 Long타입인 경우 뒤에 L을 붙여줌
+        if (optionalCustomer.isPresent()) {
+            Customer customer = optionalCustomer.get();
+            assertThat(customer.getId()).isEqualTo(1L);
+        }else{
+            System.out.println("Customer Not Found");
+        }
+        //ifPresent(Consumer)
+        //Consumer의 추상메서드는 void accept(T t)
+        optionalCustomer.ifPresent(customer -> System.out.println(customer.getCustomerName()));
+
+        //orElseGet(Supplier)
+        //Supplier의 추상메서드는 T get()
+        Customer existCustomer = customerRepository.findById(2L).orElseGet(() -> new Customer());
+        assertThat(existCustomer.getId()).isNull();
+        //assertThat(existCustomer.getId()).isEqualTo(2L);
+    }
+
+    //2-1. Customer 조회 - 정상적이지 않은경우
+    @Test
+    void testFindByNotFound(){
+        //orElseGet(Supplier)
+        //Supplier의 추상메서드는 T get()
+        Customer existCustomer = customerRepository.findById(2L)
+                .orElseGet(() -> new Customer());  //조회 값이 없으면 빈 인자를 반환
+        assertThat(existCustomer.getId()).isNull();
+        //assertThat(existCustomer.getId()).isEqualTo(2L);
+
+        //public <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier)
+        Customer notFoundCustomer = customerRepository.findById(3L)
+                .orElseThrow(() -> new RuntimeException("Customer Not Found")); //조회 값이 없으면 RuntimeException 반환 있으면 Customer 반환
+    }
 }
