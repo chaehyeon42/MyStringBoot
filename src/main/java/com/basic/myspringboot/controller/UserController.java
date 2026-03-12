da48d43a40d646e00437af7755bdbc4650fc7597
chaehyeon42 <qpqp8899@naver.com>
2026-03-12 10:16:13 +0900

    add-user

---
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.basic.myspringboot.controller;
+
+import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserRepository userRepository;
+
+    //static/index.html => UserController의 leaf() 호출 => templates/leaf.html
+    @GetMapping("/thymeleaf")
+    public String leaf(Model model) {
+        model.addAttribute("name","스프링부트");
+        return "leaf";
+    }
+
+    //방법1.
+    @GetMapping("/index")
+    public ModelAndView index(){
+        List<User> userList = userRepository.findAll();
+        return new ModelAndView("index","users",userList);
+    }
+
+    //방법2.
+    //    @GetMapping("/index")
+//    public String index(Model model) {
+//        model.addAttribute("users", userRepository.findAll());
+//        return "index";
+//    }
+
+    //등록 페이지를 Load하는 메서드
+    @GetMapping("/signup")
+    public String showSignUpForm(@ModelAttribute("userForm") User user) {
+        return "add-user";
+    }
+}
